d19f59214e71bbd8e64c1700296b40c7c8916b11
Louis GAUTHIER <lgauthier@ac-ndsf.fr>
2025-12-08 16:38:30 +0100

    Commit dictionnaire.cs

---
+++ algo_projet_final/Dictionnaire.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algo_projet_final
+{
+    internal class Dictionnaire
+    {
+        private string file_path;
+        private string[] mots;
+        private int numMots;
+        
+        public Dictionnaire(string file_path)
+        {
+            //On sauvegarde le chemin du fichier
+            this.file_path = file_path;
+
+            // On charge les mots du fichier
+            StreamReader reader = new StreamReader(file_path);
+
+            // On compte le nombre de mots dans le fichier
+            numMots = 0;
+
+            while (!reader.EndOfStream)
+            {
+                // Pour chaque ligne, on sépare les mots et on les ajoute à la liste
+                string ligne = reader.ReadLine();
+                string[] mots_ligne = ligne.Split(' ');
+
+                foreach (var mot in mots_ligne)
+                {
+                    numMots++;
+                }
+            }
+
+            mots = new string[numMots];
+            int k = 0;
+            while(!reader.EndOfStream)
+            {
+                // Pour chaque ligne, on sépare les mots et on les ajoute à la liste
+                string ligne = reader.ReadLine();
+                string[] mots_ligne = ligne.Split(' ');
+
+                foreach (var mot in mots_ligne)
+                {
+                    mots[k] = mot;
+                    k++;
+                }
+            }
+
+            // On met fin au stream
+            reader.Close();
+        }
+
+        public void Tri_quick_sor(int debut = 0, int fin = -1)
+        {
+            // On vérifie si il y a une erreur, si il faut initialiser fin et si le tableau est déjà trié
+            if (fin == -1) fin = numMots - 1;
+            if (debut >= fin || debut < 0) return;
+
+            // On choisit le pivot
+            int pivot_index = (debut + fin) / 2;
+
+            // On crée une table temporaire pour le pivot
+            string[] temp_tab = new string[fin - debut + 1];
+            int index_debut = 0;
+            int index_fin = fin - debut;
+
+            for (int i = debut; i <= fin; i++)
+            {
+                if (i == pivot_index) continue;
+
+                if (string.Compare(mots[i], mots[pivot_index]) < 0)
+                {
+                    // on ajoute au début du tableau temporaire
+                    temp_tab[index_debut] = mots[i];
+                    index_debut++;
+                }
+                else
+                {
+                    // on ajoute à la fin du tableau temporaire
+                    temp_tab[index_fin] = mots[i];
+                    index_fin--;
+                }
+            }
+
+            // On place le pivot à la bonne position
+            temp_tab[index_debut] = mots[pivot_index];
+
+            // On recopie le tableau temporaire dans le tableau principal
+            for (int i = debut; i <= fin; i++)
+            {
+                mots[i] = temp_tab[i - debut];
+            }
+
+            // On trie les deux sous-tableaux
+            Tri_quick_sor(debut, debut + index_debut - 1);
+            Tri_quick_sor(debut + index_debut + 1, fin);
+        }
+    }
+}
