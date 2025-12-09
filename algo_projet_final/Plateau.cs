1ae17b10d0aaabf1ba5de00ccab3457a8b6a3530
WWArteekxy <sallllade@gmail.com>
2025-12-09 09:22:29 +0100

    plateau retravaillé

--- algo_projet_final/Plateau.cs
+++ algo_projet_final/Plateau.cs
@@ -8,6 +8,22 @@ namespace algo_projet_final
 {
     internal class Plateau
     {
+        public char[,] Grille;
+        public int nombrecol;
+        public int nombrelig;
+        private Dictionary<char, int> maxlettre;
 
+
+        //Constructeur 
+        public Plateau(int lignes, int colonnes, string cheminFichierLettres)
+        {
+            nombrelig = lignes;
+            nombrecol = colonnes;
+            Grille = new char[lignes, colonnes];
+            maxlettre = new Dictionary<char, int>();
+            Dictionary<char, int> compteurLettre = new Dictionary<char, int>();
+
+
+        }
     }
 }
